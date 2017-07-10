f90a677df907bef85c8c64fe577ced7f3eaca60a
Neha Khan [Consultant to Tower] <nkhan@tower-research.com>
2017-07-10 14:49:30 -0400

    cleaning up
    Change-Id: I37b0bd6b956678cc8ced0ec66107532a2e018acb

--- src/steps.py
+++ src/steps.py
@@ -2,7 +2,6 @@ from lettuce import step, world
 from hello_world import say_hello, ACQANTAINCES, MASTER
 
 
-
 def _get_expected_response(name):
     if name:
         if name in ACQANTAINCES:
@@ -17,14 +16,17 @@ def address_with_a_name(step, name):
     world.name = name
     world.expected_response = _get_expected_response(name)
 
+
 @step('Alexa is addressed anonymously')
 def address_anonymously(step):
     world.name = None
     world.expected_response = "Whatever"
 
+
 @step('Alexa activates')
 def invoke_alexa_greet_func(step):
     world.actual_response = say_hello(world.name)
+
 
 @step('Alexa responds (\S+)')
 def compare_response(step, *args):
