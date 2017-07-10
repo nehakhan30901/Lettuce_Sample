3ce6a6e92c003ee0c951a76cbfc222261071e2fa
Neha Khan [Consultant to Tower] <nkhan@tower-research.com>
2017-07-10 15:32:43 -0400

    Fixed
    Change-Id: I4bff2b57cc4a3e47cf727563ac75b23c99d68537

--- src/hello_world.feature
+++ src/hello_world.feature
@@ -1,23 +1,23 @@
 Feature: Testing greeting behaviour of Alexa
 
-    Scenario: Alexa identifies its master and greet accordingly
+    Scenario:  Alexa identifies its master and greet accordingly
         Given  Neha addresses Alexa
         When   Alexa activates
         Then   Alexa responds Hello Master Neha
 
 
-    Scenario: Alexa identifies friends and greet accordingly
+    Scenario:  Alexa identifies friends and greet accordingly
         Given  Shubham addresses Alexa
         When   Alexa activates
-        Then   Alexa responds Hello Friend Neha
+        Then   Alexa responds Hello Friend Shubham
 
 
-    Scenario: Alexa greets a stranger
+    Scenario:  Alexa greets a stranger
         Given  Tanushree addresses Alexa
         When   Alexa activates
         Then   Alexa responds Hello Stranger
 
-    Scenario: Alexa is not nice to anonymous calls
+    Scenario:  Alexa is not nice to anonymous calls
         Given  Alexa is addressed anonymously
         When   Alexa activates
         Then   Alexa responds Whatever
--- src/steps.py
+++ src/steps.py
@@ -1,21 +1,10 @@
 from lettuce import step, world
-from hello_world import say_hello, ACQANTAINCES, MASTER
-
-
-def _get_expected_response(name):
-    if name:
-        if name in ACQANTAINCES:
-            return "Hello Friend {0}".format(name)
-        if name in MASTER:
-            return "Hello Master {0}".format(name)
-        return "Hello Stranger"
+from hello_world import say_hello
 
 
 @step('(\S+) addresses Alexa')
 def address_with_a_name(step, name):
     world.name = name
-    world.expected_response = _get_expected_response(name)
-
 
 @step('Alexa is addressed anonymously')
 def address_anonymously(step):
@@ -28,10 +17,7 @@ def invoke_alexa_greet_func(step):
     world.actual_response = say_hello(world.name)
 
 
-@step('Alexa responds (\S+)')
-def compare_response(step, *args):
-    return world.expected_response == world.actual_response
-
-
-
+@step('Alexa responds ([\s\S]+)')
+def compare_response(step, expected_response):
+        assert world.actual_response == expected_response, "Got {0} needed {1}".format(world.actual_response, expected_response)
 
